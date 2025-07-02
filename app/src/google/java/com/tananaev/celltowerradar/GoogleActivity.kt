fa53933c02670a46bb3a4758ff865d3bc2e71792
Anton Tananaev <anton.tananaev@gmail.com>
2025-07-02 09:15:29 -0700

    Add insets

--- app/src/google/java/com/tananaev/celltowerradar/GoogleActivity.kt
+++ app/src/google/java/com/tananaev/celltowerradar/GoogleActivity.kt
@@ -2,7 +2,11 @@ package com.tananaev.celltowerradar
 
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
@@ -25,7 +29,15 @@ class GoogleActivity : AppCompatActivity() {
                 .add(R.id.map, MainFragment())
                 .commit()
         }
-        findViewById<AdView>(R.id.ad_view).loadAd(AdRequest.Builder().build())
+        val adView = findViewById<AdView>(R.id.ad_view)
+        adView.loadAd(AdRequest.Builder().build())
+        ViewCompat.setOnApplyWindowInsetsListener(adView) { v, insets ->
+            val bottomInset = insets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom
+            v.updateLayoutParams<LinearLayout.LayoutParams> {
+                bottomMargin = bottomInset
+            }
+            insets
+        }
     }
 
     override fun onResume() {
